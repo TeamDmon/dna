906a4c562ab63e294ac3b7f8600e0b2fe65dbb64
kucerajacob <kucerajacob@gmail.com>
2018-02-18 17:53:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,20 +18,11 @@ client.on('message', (message) => {
         message.reply("Your discord is " + message.author.username + "#" + message.author.discriminator);
     }
     
-    if (message.content == '!help') {
-        message.channel.sendMessage('**DNA Bot (Commands)** \n !bunkerlink - ???');
-    }
-    
-    
     // Banned Words
     
     msg = message.content.toLowerCase(); // Ignore Case
     
     if (msg.includes('nigger') || msg.includes('niggers')) {
-        message.delete(message);
-    }
-    
-    if (msg.includes('kike') || msg.includes('kikes')) {
         message.delete(message);
     }
     
